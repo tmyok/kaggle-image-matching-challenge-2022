4fcec1c73b07e097fb9cf927e729ac03142759d8
Tomoya Okazaki <tomoya.okazaki@konicaminolta.com>
2022-07-03 17:19:25 +0900

    Update docker_container.sh

--- docker_container.sh
+++ docker_container.sh
@@ -3,8 +3,9 @@
 
 docker run \
     -it \
+    --rm \
     --gpus all \
-    --name kaggle_IMC2022_$1 \
+    --name kaggle_IMC2022 \
     --volume $(pwd)/:/home/work/ \
     --workdir /home/work/working/ \
     tmyok/pytorch:1.11.0-cuda11.6.2-cudnn8-opencv460-ubuntu20.04
